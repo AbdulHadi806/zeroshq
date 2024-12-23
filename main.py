a31c33ca627184cace9cf6417fd0d661faab8fe0
Abdul Haadi <abdul.806@yahoo.com>
2024-12-23 09:50:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pandas as pd
 
-keywords_df = pd.read_csv('Sample Keywords For Hadi Demo-2.csv')
+keywords_df = pd.read_csv('keywordfile.csv')
 
-article = """
-Hyperwrite ensures my personal resources are being routed to where they’re needed the most. Yes, I still struggle with Writer’s Block. I think that’s a given in this industry that I’ve chosen. But trust me, streamlining the technical details of the writing process helps a ton.
-With a powerful AI assistant doing most of the heavy lifting during content ideation, research, and information organization, it’s so much easier to stay focused and inspired on the task.
-
-"""
+article = """Add the article here"""
 
 def count_keyword_occurrences(keyword, text):
     return text.lower().count(keyword.lower())
